6a17cf0b120c9652eb83fd25f560b8ca2dec2118
Filipe Oliveira <filipeoliveira@filipsol.com>
2026-06-18 16:41:27 +0100

    Creating and Merging Development Config

---
+++ marketing/config/webpack.dev.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const commonConfig = require('./webpack.common');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8081, // specify the port for the development server
+        historyApiFallback: {
+            index: '/index.html' // fallback to index.html for single-page applications
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html' // specify the template for the HTML file
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig); // merge the common configuration with the development configuration
