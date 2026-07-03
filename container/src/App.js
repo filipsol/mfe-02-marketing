57bdb622495a405d0616ba8d1870444ffcf1e8b0
Filipe Oliveira <filipeoliveira@filipsol.com>
2026-07-03 10:52:58 +0100

    Integration of the Container and Marketing

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { mount } from 'marketing/MarketingApp';
+
+// console.log(mount);
 
 export default () => {
-  return (
-    <div>
-        <h1>Container</h1>
-        <p>This is the container application.</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Container</h1>
+            <p>This is the container application.</p>
+        </div>
+    );
 }
--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
@@ -14,6 +14,13 @@ const devConfig = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html' // specify the template for the HTML file
+        }),
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            }
         })
     ]
 }
