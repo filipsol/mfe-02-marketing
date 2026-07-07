4cbe0e4f24794060485ba9937ba9f93beba8fba7
Filipe Oliveira <filipeoliveira@filipsol.com>
2026-07-07 13:02:29 +0100

    Webpacks PublishPath Setting

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js', // Cache busting for production
+    publicPath: '/container/latest/', // Set the public path for production
   },
   plugins: [
     new ModuleFederationPlugin({
