43f87833afaaf75d6f94fd844317172085fc5b66
Filipe Oliveira <filipeoliveira@filipsol.com>
2026-07-07 15:05:00 +0100

    Adding Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 // import { mount } from 'marketing/MarketingApp'; // import the mount function from the MarketingApp exposed by the marketing microfrontend
 // console.log(mount);
 import MarketingApp from './components/MarketingApp'; // import the MarketingApp component
+import Header from './components/Header';
 
 export default () => {
     return (
-        <div>
-            <h1>Container!!</h1>
-            <hr />
-            <MarketingApp />
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
     );
 }
