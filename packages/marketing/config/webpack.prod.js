681c1cde51dc0f8224f5c831d54bce4f0f5b61dc
Filipe Oliveira <filipeoliveira@filipsol.com>
2026-07-07 14:55:34 +0100

    Setting Up the Marketing Deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,6 +7,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js', // Cache busting for production
+    publicPath: '/marketing/latest/', // Set the public path for production
   },
   plugins: [
     new ModuleFederationPlugin({
