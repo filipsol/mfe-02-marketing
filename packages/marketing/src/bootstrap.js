f409bf88d56b41c70795df6eafa1c6db4cd41540
Filipe Oliveira <filipeoliveira@filipsol.com>
2026-07-08 10:10:07 +0100

    Simple Test

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -11,13 +11,13 @@ const mount = (el) => {
 
 // if we are in development and in isolation
 // call mount immediately
-if (process.env.NODE_ENV === 'development') {
+// if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if (devRoot) {
         mount(devRoot);
     }
-}
+// }
 
 // we are running through container
 // and we should export the mount function
