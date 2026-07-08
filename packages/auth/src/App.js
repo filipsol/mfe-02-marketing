fdf3ba85cd553e4bf72c46cbf2c5e2ae7a9b45f4
Filipe Oliveira <filipeoliveira@filipsol.com>
2026-07-08 17:11:21 +0100

    Adding Signin and Singup Forms

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import {Route, Switch, Router } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+
 
 export default ({ history }) => {
 
@@ -15,7 +18,8 @@ export default ({ history }) => {
             <StylesProvider generateClassName={generateClassName}>
                 <Router history={history}>
                     <Switch>
-                        
+                        <Route path="/auth/signin" component={SignIn} />
+                        <Route path="/auth/signup" component={SignUp} />
                     </Switch>
                 </Router>
             </StylesProvider>
