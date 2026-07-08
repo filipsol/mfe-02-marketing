b6b59d2fcbc5c20cea812803db91e024cfd08f3a
Filipe Oliveira <filipeoliveira@filipsol.com>
2026-07-08 18:37:39 +0100

    Adding a Loading Bar

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,6 +5,7 @@ import { StylesProvider, createGenerateClassName } from '@material-ui/core/style
 // console.log(mount);
 // import MarketingApp from './components/MarketingApp'; // import the MarketingApp component
 // import AuthApp from './components/AuthApp'; // import the AuthApp component
+import Progress from './components/Progress'; // import the Progress component
 import Header from './components/Header';
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp')); // lazy load the MarketingApp component
@@ -20,7 +21,7 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
-                    <Suspense fallback={<div>Loading...</div>}>
+                    <Suspense fallback={<Progress />}>
                         <Switch>
                             <Route path="/auth" component={AuthLazy} />
                             <Route path="/" component={MarketingLazy} />
