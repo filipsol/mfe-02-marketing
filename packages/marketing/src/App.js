7be296b197f19ade1bf84cd496a68344def4b0c1
Filipe Oliveira <filipeoliveira@filipsol.com>
2026-07-08 10:22:12 +0100

    Simple Test

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, MemoryRouter, Route, Switch } from "react-router-dom";
 import { StylesProvider } from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
 export default () => {
+    const path = window.location.pathname;
+    const isStandaloneMarketingPath = path.startsWith('/marketing/latest') || path === '/';
+    const Router = isStandaloneMarketingPath ? BrowserRouter : MemoryRouter;
+
     return (
         <div>
             <StylesProvider>
-                <BrowserRouter>
+                <Router>
                     <Switch>
                         <Route path="/pricing" component={Pricing} />
                         <Route exact path="/" component={Landing} />
                     </Switch>
-                </BrowserRouter>
+                </Router>
             </StylesProvider>
         </div>
     );
