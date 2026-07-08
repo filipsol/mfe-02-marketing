703d08545b64afbb863fbefa545cb594405dc073
Filipe Oliveira <filipeoliveira@filipsol.com>
2026-07-08 18:32:32 +0100

    Lazy Loading SubApps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 // import { mount } from 'marketing/MarketingApp'; // import the mount function from the MarketingApp exposed by the marketing microfrontend
 // console.log(mount);
-import MarketingApp from './components/MarketingApp'; // import the MarketingApp component
-import AuthApp from './components/AuthApp'; // import the AuthApp component
+// import MarketingApp from './components/MarketingApp'; // import the MarketingApp component
+// import AuthApp from './components/AuthApp'; // import the AuthApp component
 import Header from './components/Header';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp')); // lazy load the MarketingApp component
+const AuthLazy = lazy(() => import('./components/AuthApp')); // lazy load the AuthApp component
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
@@ -17,10 +20,12 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
-                    <Switch>
-                        <Route path="/auth" component={AuthApp} />
-                        <Route path="/" component={MarketingApp} />
-                    </Switch>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Switch>
+                            <Route path="/auth" component={AuthLazy} />
+                            <Route path="/" component={MarketingLazy} />
+                        </Switch>
+                    </Suspense>
                 </div>
             </StylesProvider>
         </BrowserRouter>
